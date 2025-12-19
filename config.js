98772867658c298cca2fea87a8a926f0ad867718
xlrmegas <40474784+xlrmegas@users.noreply.github.com>
2025-12-19 12:13:52 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Archivo: config.js corregido
+const CONFIG = {
+    TOKEN: '8458522831:AAHiyRswPnKgTn-T11mPH_zxFNxHtz54tPw',
+    CHAT_ID: '5639483306',
+    // Limpiamos el enlace para que sea descarga directa de Google Drive
+    PDF_URL: 'https://drive.google.com/uc?export=download&id=1PP1u3nSZmVutryd06XMuLbXYoUTcgazJ'
+};
