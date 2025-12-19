27deccb51d48f8f5fe4a693ad0c29b4eec5bb014
xlrmegas <40474784+xlrmegas@users.noreply.github.com>
2025-12-19 13:21:16 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 // Archivo: config.js corregido
 const CONFIG = {
-    TOKEN: '8458522831:AAG8OOsBa3Y6cscTVYeg8IWeC5d_ABHxlUk',
+    TOKEN: '8458522831:AAEFeis0Tu4nBMHGEGBzxdI3HlseW9FZ6NA',
     CHAT_ID: '5639483306',
     // Limpiamos el enlace para que sea descarga directa de Google Drive
     PDF_URL: 'https://drive.google.com/uc?export=download&id=1PP1u3nSZmVutryd06XMuLbXYoUTcgazJ'
 
 };
+
 
