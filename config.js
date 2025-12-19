1862350f5e696ca649f81bb2749d551875caf447
xlrmegas <40474784+xlrmegas@users.noreply.github.com>
2025-12-19 17:10:44 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-    // Fíjate en el atob(...) es vital para que funcione
-    TOKEN: atob('ODQ1ODUyMjgzMTpBQUVWUVBTSkpkZlpyVUJMZGNOOUlmcDZGeVlZZWtEM093OA=='),
-    CHAT_ID: '5639483306',
-    PDF_URL: 'https://drive.google.com/uc?export=download&id=1PP1u3nSZmVutryd06XMuLbXYoUTcgazJ'
-};
-
